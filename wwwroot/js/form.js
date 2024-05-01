e026fc576d40aa837c3470d967d569e9d16501f3
Hakan Arslan <128188577+Hkn36Arslan@users.noreply.github.com>
2024-05-01 11:06:47 +0300

    form.js dosyası içerisinde tarih alanı ve tc kimlik no imput alanı için güncelleme yapıldı.

--- wwwroot/js/form.js
+++ wwwroot/js/form.js
@@ -5,51 +5,26 @@ window.onload = function () {
   var ay = simdikiTarih.getMonth() + 1;
   var yil = simdikiTarih.getFullYear();
   if (ay < 10) {
-    document.getElementById("date").innerText =
+    if(gun<10){
+      document.getElementById("date").innerText =
+      "0" + gun + "/" + "0" + ay + "/" + yil;
+    }
+    else{
+      document.getElementById("date").innerText =
       gun + "/" + "0" + ay + "/" + yil;
+    }
+    
   } else {
-    document.getElementById("date").innerText = gun + "/" + ay + "/" + yil;
+    if(gun<10){
+      document.getElementById("date").innerText = "0" + gun + "/" + ay + "/" + yil;
+    }
+    else{
+      document.getElementById("date").innerText = gun + "/" + ay + "/" + yil;
+    }
   }
 };
 
-// function showSignaturePreview(event) {
-//   const file = event.target.files[0];
-//   const reader = new FileReader();
-
-//   reader.onload = function (event) {
-//     const signaturePreview = document.getElementById("signaturePreview");
-//     signaturePreview.src = event.target.result;
-//     var imzaInput=document.getElementById("signatureInput");
-//     imzaInput.style.display="none";
-//     signaturePreview.style.display = "block";
-//   };
-//   reader.readAsDataURL(file);
-// }
-
-/* DersTablosu Hücre Ekleme */
-// document.getElementById('ekleButton').addEventListener('click', function() {
-//   event.preventDefault();
-//   var derslerBody = document.getElementById('derslerBody');
-//   var newRow = document.createElement('tr');
-
-//   // Dersler için gerekli input tiplerini tanımla
-//   var inputTypes = ['text', 'number', 'number', 'text', 'text', 'number', 'number', 'text'];
-
-//   for (var i = 0; i < 8; i++) {
-//     var cell = document.createElement('td');
-//     var input = document.createElement('input');
-
-//     // Input tipini belirle
-//     input.setAttribute('type', inputTypes[i]);
-//     input.setAttribute('class', 'inputDers');
-
-//     cell.appendChild(input);
-//     newRow.appendChild(cell);
-//   }
-
-//   derslerBody.appendChild(newRow);
-// });
-
+/* ! Ders Tablosuna yeni satır ekleme  */
 document
   .getElementById("ekleButton")
   .addEventListener("click", function (event) {
@@ -100,3 +75,24 @@ document
 
     derslerBody.appendChild(newRow);
   });
+
+  /* ! T.C. kimlik no kontrol */
+
+  var inputElement = document.getElementById("tc_no");
+
+  inputElement.addEventListener("input", function () {
+         
+      var value = this.value;
+
+       if (value.length != 11 || !isNumeric(value)) {
+                this.setCustomValidity("TC Kimlik numarası 11 haneli olmalı ve sadece rakamlardan oluşmalıdır.");
+       } else {
+                // Hata mesajını temizle
+                this.setCustomValidity("");
+       }
+  });
+
+  // Girilen değerin sadece rakamlardan oluşup oluşmadığını kontrol etmek için fonksiyon
+  function isNumeric(str) {
+     return /^\d+$/.test(str);
+  }
