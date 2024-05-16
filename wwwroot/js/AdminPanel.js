7b604fdf4c243dc06d5a309670b4442b195adf1d
Hakan Arslan <128188577+Hkn36Arslan@users.noreply.github.com>
2024-05-16 19:35:23 +0300

    Admin panelde butonlar, fonksiyonlar noktasında düzenlemeler yapıldı

--- wwwroot/js/AdminPanel.js
+++ wwwroot/js/AdminPanel.js
@@ -21,13 +21,18 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     
     ekleButton.addEventListener("click", function() {
+
         var kayitFormPanel = document.querySelector(".kayitFormPanel");
         var kullaniciTable = document.querySelector(".kullaniciTable");
+        var inputFields = kayitFormPanel.querySelectorAll("input");
 
         kullaniciTable.style.display = "none";
-        kayitFormPanel.style.display = "block";  
+        kayitFormPanel.style.display = "block"; 
+        inputFields.forEach(function(input) {
+            input.value = "";
+        }); 
+       
     });
-
     registerForm.addEventListener("submit", function(event) {
         event.preventDefault(); 
         
@@ -38,5 +43,33 @@ document.addEventListener("DOMContentLoaded", function() {
         kayitFormPanel.style.display = "none";
           
     });
+
+    document.querySelector("#kayitFormPanel-close").addEventListener("click", function() {
+        
+        var kullaniciTable = document.querySelector(".kullaniciTable");
+        var kayitFormPanel = document.querySelector(".kayitFormPanel");
+        
+        kullaniciTable.style.display = "block";
+        kayitFormPanel.style.display = "none";
+          
+    });
+    document.querySelector("#kullaniciPanel-close").addEventListener("click", function() {
+        
+        var kullaniciPanel = document.querySelector("#kullaniciPanel");
+        var kayitFormPanel = document.querySelector(".kayitFormPanel");
+        kayitFormPanel.style.display = "none";
+        kullaniciPanel.style.display = "none";
+          
+    });
+    document.querySelector("#basvuruPanel-close").addEventListener("click", function() {
+        
+        var kullaniciPanel = document.querySelector("#kullaniciPanel");
+        var basvuruPanel = document.querySelector("#basvuruPanel");
+
+        basvuruPanel.style.display = "none";
+        kullaniciPanel.style.display = "none";
+          
+    });
+
 });
 
