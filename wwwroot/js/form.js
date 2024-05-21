04605560411ff34347fe9f1ef585d58e391380e6
Hakan Arslan <128188577+Hkn36Arslan@users.noreply.github.com>
2024-05-21 20:02:47 +0300

    Genel düzenleme ve pdf sorunları düzeltildi

--- wwwroot/js/form.js
+++ wwwroot/js/form.js
@@ -76,8 +76,8 @@ document.getElementById("ekleButton").addEventListener("click", function (event)
 });
 
 var form = document.querySelector('form');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+form.addEventListener('submit', function () {
+    
     var formData = new FormData(form); // Form verilerini al
 
     var bolumBaskanligiValue = document.getElementById("bolum_baskanligi").value;
