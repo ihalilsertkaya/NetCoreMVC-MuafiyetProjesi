90aae7afa21d2137ddeea955bad7c6ed3ac36966
Hakan Arslan <128188577+Hkn36Arslan@users.noreply.github.com>
2024-05-21 18:24:54 +0300

    form.js dosyasında düzenleme yapıldı

--- wwwroot/js/form.js
+++ wwwroot/js/form.js
@@ -76,8 +76,9 @@ document.getElementById("ekleButton").addEventListener("click", function (event)
 });
 
 var form = document.querySelector('form');
-form.addEventListener('submit', function () {
-
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
     var formData = new FormData(form); // Form verilerini al
 
     var bolumBaskanligiValue = document.getElementById("bolum_baskanligi").value;
