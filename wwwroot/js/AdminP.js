e1984c4bd92cc11700735e6989d4941d2c82d423
hakan.arslan84 <hakan.arslan84@erzurum.edu.tr>
2024-05-27 23:30:02 +0000

    Admin.js güncellendi.

--- wwwroot/js/AdminP.js
+++ wwwroot/js/AdminP.js
@@ -111,38 +111,42 @@ document.addEventListener("DOMContentLoaded", function () {
             row.style.display = (filterValue === "" || department.includes(filterValue)) ? "" : "none";
         });
     });
-    var basvuruTable = document.querySelector('#basvuruTable');
+    var basvuruTable = document.querySelector("#basvuruTable");
 
-    basvuruTable.addEventListener('click', function (event) {
+    basvuruTable.addEventListener("click", function (event) {
         // Sadece PDF bağlantılarına tıklamaları dinlemek için
-        if (event.target.matches('.fa-file-pdf')) {
-            var tr = event.target.closest('tr');
-            
-            // Tıklanan öğeye göre ilgili veri niteliğini ayarla
-            if (event.target.id === 'Basvuru') {
-                tr.dataset.basvuruAcildi = "true";
-                event.target.style.color = 'black'; 
-            } else if (event.target.id === 'Transkript') {
-                event.target.style.color = 'black'; 
-                tr.dataset.transkriptAcildi = "true";
-            } else if (event.target.id === 'DersDokum') {
-                event.target.style.color = 'black'; 
-                tr.dataset.dersDokumAcildi = "true";
-            }
+        if (event.target.matches(".fa-file-pdf")) {
+        var tr = event.target.closest("tr");
 
-            // Tüm bağlantıların tıklanıp tıklanmadığını kontrol et
-            checkAllOpened(tr);
+        // Tıklanan öğeye göre ilgili veri niteliğini ayarla
+        if (event.target.id === "Basvuru") {
+            tr.dataset.basvuruAcildi = "true";
+            event.target.style.color = "black";
+        } else if (event.target.id === "Transkript") {
+            event.target.style.color = "black";
+            tr.dataset.transkriptAcildi = "true";
+        } else if (event.target.id === "DersDokum") {
+            event.target.style.color = "black";
+            tr.dataset.dersDokumAcildi = "true";
+        }
+
+        // Tüm bağlantıların tıklanıp tıklanmadığını kontrol et
+        checkAllOpened(tr);
         }
     });
 
     function checkAllOpened(tr) {
-        if (tr.dataset.basvuruAcildi === "true" && tr.dataset.transkriptAcildi === "true" && tr.dataset.dersDokumAcildi === "true") {
-            tr.style.backgroundColor = '#24D330';
-            tr.style.color = 'white';
-            
-            tr.querySelectorAll(".fa-file-pdf").forEach(function(pdfIcon) {
-                pdfIcon.style.color = "white";
-            });
+        if (
+        tr.dataset.basvuruAcildi === "true" &&
+        tr.dataset.transkriptAcildi === "true" &&
+        tr.dataset.dersDokumAcildi === "true"
+        ) {
+        tr.style.backgroundColor = "#24D330";
+        tr.style.color = "white";
+        
+        tr.querySelectorAll(".fa-file-pdf").forEach(function(pdfIcon) {
+            pdfIcon.style.color = "white";
+        });
         }
     }
 });
