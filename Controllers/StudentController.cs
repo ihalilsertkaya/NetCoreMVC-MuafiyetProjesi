cee088c55233f6211291eb63d35557e7e4d6711d
Abdulhalik Akdeniz <abdul_halik@outlook.com>
2024-05-27 18:07:17 +0300

    StudentController içerisinde düzeltme yapıldı

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -73,7 +73,7 @@ namespace MuafiyetProjesi2024.Controllers
                     evrakBilgisi.DersIcerik = dersDokumDosyaYolu;
                 }
 
-                var oturumuAcanTc = TempData["oturumAcanTc"] as String;
+                var oturumuAcanTc = TempData.Peek("oturumAcanTc") as String;
                 var oturumuAcankullanici = _context.Kullanicilar.SingleOrDefault(x => x.Tckimlik == oturumuAcanTc);
                 
                 basvuruBilgisi.Kullanici = oturumuAcankullanici;
