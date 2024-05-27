fa7f1262136dc7fa93e335c260ba50401f0ba96c
İbrahim Halil Sertkaya <ibrahim.sertkaya82@erzurum.edu.tr>
2024-05-27 11:06:09 +0000

    Revert "Home controller deneme1"
    This reverts commit b11bf509b4dd641e8f93c781a4f61220e059b367

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -137,7 +137,7 @@ namespace MuafiyetProjesi2024.Controllers
 
             // Dosya adını ve yolunu belirle
             string fileName = $"{ogrTC}-Basvuru.pdf";
-            string filePath = Path.Combine("/uploads", fileName);
+            string filePath = Path.Combine("wwwroot/uploads", fileName);
 
             // PDF'yi sunucuda kaydet
             await System.IO.File.WriteAllBytesAsync(filePath, pdfBytes);
